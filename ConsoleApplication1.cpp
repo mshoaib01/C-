be555ddeb12c0d037a7b67b83298048bf260919b
mshoaib01 <muhammad.muhammadshoaib91@gmail.com>
2021-08-18 17:23:55 +0200

    I created a class with method Countit and pass 2 string variables to the constructor that set the value for class variables. One for the input string and other for the Alphabet that need to be search

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,37 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class CountAlphabets
+{
+public:    // Access specifier         
+   string inputStringForCount;
+   string alphabetToSearch;
+   int count = 0;
+   CountAlphabets(string inputString, string inputString2)
+   { // Constructor with parameters
+       inputStringForCount = inputString;
+       alphabetToSearch = inputString2;
+   }
+
+   void  Countit() // function count
+   {  
+     for (int i = 0; i < inputStringForCount.length(); i++) 
+     {
+         if (inputStringForCount[i] == alphabetToSearch[0])
+         {
+             count++;
+         }
+     }
+
+     cout << "You have: " << count << " '" << alphabetToSearch << "' alphabet in the string \n";
+   }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CountAlphabets myObj("Und die Ruhe, mit der Kossakosvky hier vorgeht, provoziert all die Gedanken, die einem in gewöhnlichen Dokumentationen ein Off-Text einflüstern würde","o");  // Create an object of MyClass
+    myObj.Countit();
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ CountAlphabets.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class CountAlphabets
+{
+public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
+
